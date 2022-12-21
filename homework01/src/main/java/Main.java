60f0d567d17748dd28849aba6420265a698bf25f
Lyonin Nikita <nekitlenin@gmail.com>
2022-12-22 00:06:44 +0300

    add homework01

---
+++ homework01/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Project: Maxima_01
+ *
+ * @author nikita
+ * Email:  nekitlenin@gmail.com |
+ * Create: 21.12.2022 22:34 |
+ * Created with IntelliJ IDEA
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
